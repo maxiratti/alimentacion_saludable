7ce6bcee8131266203f781b96ac177fda37f9bd9
Maximiliano Ratti <telcom.web@gmail.com>
2025-11-21 22:18:59 -0300

    Primer subida del proyecto

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+function mostrar1(){
+     document.getElementById('contenido').style.display ='block'
+}
+function ocultar1(){
+    document.getElementById('contenido').style.display ='none'
+}
+function mostrar(){
+    document.getElementById('second4videos').style.display ='block'
+}
+function ocultar(){
+   document.getElementById('second4videos').style.display ='none'
+}
